d6e2ac5289cbb96d8e6f556dbf7e8866192b7d16
Jismi Jose <jismitjose23@gmail.com>
2024-10-21 22:12:03 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCProject_2311248.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
